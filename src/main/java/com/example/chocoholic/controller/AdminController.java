a07071f19f20341f00da485b70bfdfd35f01b01c
adarshrwl <adarshrawal321@gmail.com>
2023-07-17 03:48:22 +0545

    Admin Task Complete

--- src/main/java/com/example/chocoholic/controller/AdminController.java
+++ src/main/java/com/example/chocoholic/controller/AdminController.java
@@ -1,22 +1,59 @@
 package com.example.chocoholic.controller;
-
+import com.example.chocoholic.model.Category;
+import com.example.chocoholic.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class AdminController {
+    @Autowired
+    CategoryService categoryService;
+
     @GetMapping("/admin")
     public String adminHome(){
         return "adminHome";
     }
 
     @GetMapping("/admin/categories")
-    public String getCat(){
+    public String getCat(Model model){
+        model.addAttribute("categories",categoryService.getAllCategory());
         return "categories";
     }
     @GetMapping("/admin/categories/add")
-    public  String addCate(){
+    public  String addCate(Model model){
+        model.addAttribute("categories",new Category());
         return "categoriesAdd";
     }
+    @PostMapping ("/admin/categories/add")
+    public  String postCate(@ModelAttribute("categories")Category category){
+        categoryService.addCategory(category);
+        return "redirect:/admin/categories";
+    }
+
+    @PostMapping("/admin/categories/delete/{id}")
+    public String deleteCate(@PathVariable int id) {
+        categoryService.removeCategoryById(id);
+        return "redirect:/admin/categories";
+    }
+
+
+    @GetMapping("/admin/categories/update/{id}")
+    public String updateCate(@PathVariable int id, Model model) {
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if (category.isPresent()) {
+            model.addAttribute("categories", category.get());
+            return "categoriesAdd";
+        } else {
+            return "404";
+        }
+    }
+
 
 }
---
+++ src/main/java/com/example/chocoholic/service/CategoryService.java
@@ -0,0 +1,29 @@
+package com.example.chocoholic.service;
+
+import com.example.chocoholic.model.Category;
+import com.example.chocoholic.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryRepository CategoryRepository;
+    public void addCategory(Category category){
+        CategoryRepository.save(category);
+    }
+    public List<Category> getAllCategory(){
+        return CategoryRepository.findAll();
+    }
+    public void removeCategoryById(int id) {
+        CategoryRepository.deleteById(id);
+    }
+
+    public Optional<Category> getCategoryById(int id){
+        return CategoryRepository.findById(id);
+    }
+
+}
