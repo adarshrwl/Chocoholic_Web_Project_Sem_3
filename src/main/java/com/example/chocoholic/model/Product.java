9bbe090323b262f7acde19b9e3c01234046a03fa
adarshrwl <adarshrawal321@gmail.com>
2023-07-20 16:59:06 +0545

    added the functionality and modified some filess

--- src/main/java/com/example/chocoholic/model/Product.java
+++ src/main/java/com/example/chocoholic/model/Product.java
@@ -3,22 +3,24 @@ package com.example.chocoholic.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+
 @Entity
 @Data
-
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "category_id")
     private long id;
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id",referencedColumnName = "category_id")
+    @JoinColumn(name = "category_id")
     private Category category;
-    private  Double price;
-    private String Description;
-//    private  double weight;
-
+    private Double price;
+    private String description;
     private String imageName;
 }
+
+//    private  double weight;
+
+
+
