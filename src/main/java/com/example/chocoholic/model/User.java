f73b505d44395827140d77b0f43fee826015b71e
adarshrwl <adarshrawal321@gmail.com>
2023-07-22 14:08:39 +0545

    Added the codes for the User Roles and Authentication

--- src/main/java/com/example/chocoholic/model/User.java
+++ src/main/java/com/example/chocoholic/model/User.java
@@ -17,12 +17,12 @@ public class User {
     private int id;
 
     @NotEmpty
-    @Column(nullable=false)
+    @Column(nullable = false)
     private String firstName;
 
     private String lastName;
 
-    @Column(nullable = false,unique = true)
+    @Column(nullable = false, unique = true)
     @NotEmpty
     @Email(message = "{errors.invalid_email}")
     private String email;
@@ -32,19 +32,22 @@ public class User {
     private String password;
 
 
-    @ManyToMany (cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role",
-            joinColumns = {@JoinColumn (name = "USER_ID", referencedColumnName = "ID")},
-                    inverseJoinColumns = {@JoinColumn (name = "ROLE_ID", referencedColumnName = "ID")}
+            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")}
     )
     private List<Role> roles;
-    public User (User user) {
+
+    public User(User user) {
         this.firstName = user.getFirstName();
         this.lastName = user.getLastName();
         this.email = user.getEmail();
         this.password = user.getPassword();
         this.roles = user.getRoles();
     }
+
     public User() {
+    }
 }
