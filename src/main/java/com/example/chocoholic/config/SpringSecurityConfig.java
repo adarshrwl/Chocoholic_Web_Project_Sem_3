cf339a130b8edb373d34d02ec2790d9af62f4e44
adarshrwl <adarshrawal321@gmail.com>
2023-07-24 00:46:14 +0545

    Completed the configuration of the project

--- src/main/java/com/example/chocoholic/config/SpringSecurityConfig.java
+++ src/main/java/com/example/chocoholic/config/SpringSecurityConfig.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -24,14 +22,15 @@ public class SpringSecurityConfig {
     protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeHttpRequests()
+                .requestMatchers("/admin").hasRole("ADMIN")
                 .requestMatchers("/", "/shop/**", "/register")
                 .permitAll()
-                .anyRequest()
+                .anyRequest( )
                 .authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/dashboard",true)
+                .defaultSuccessUrl("/",true)
                 .usernameParameter("email")
                 .passwordParameter("password")
                 .permitAll()
--- src/main/java/com/example/chocoholic/model/Role.java
+++ src/main/java/com/example/chocoholic/model/Role.java
@@ -20,5 +20,4 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
-
 }
